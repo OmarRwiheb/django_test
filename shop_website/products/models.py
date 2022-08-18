c2ce80042e7925b15230e330e3a2681f9f98bdcd
OmarRwiheb <omarrwiheb99@gmail.com>
2022-08-18 03:48:33 +0200

    commit

--- shop_website/products/models.py
+++ shop_website/products/models.py
@@ -6,6 +6,7 @@ class product(models.Model):
   description = models.TextField(blank=True)
   price = models.DecimalField(decimal_places = 2, max_digits=1000, null=True)
   summary = models.TextField(default='')
+  commit_test = models.CharField(max_length = 120)
 
   def __str__(self):
     return self.title
